c82d8f0914e1e4098940f91869fa1ba8bdcb2a65
Noxerinno <frantz.darbon@telecomnancy.net>
2024-11-12 17:50:44 +0100

    Finale closest to zero solution

--- Leetcode/algomap-io/arrays_and_strings/ClosestToZero.cpp
+++ Leetcode/algomap-io/arrays_and_strings/ClosestToZero.cpp
@@ -8,8 +8,28 @@ ClosestToZero::ClosestToZero() { }
 
 int ClosestToZero::findClosestNumber(vector<int>& nums)
 {
+	// If nums is null or empty, return 0
+	if(nums.empty())
+	{
+		return 0;
+	}
+
+	// If nums' size is 1, return the only element
+	if(nums.size() == 1)
+	{
+		return nums[0];
+	}
+
+	// Otherwise we sort the vector in relative absolute values
 	sort(nums.begin(), nums.end(), myCompare);
-	return (abs(nums[0]));
+
+	// If the closest int to 0 found is negative, we check if there is an equal positive value
+	if(nums[0] < 0 && find(nums.begin(), nums.end(), abs(nums[0])) != nums.end())
+	{
+		return (abs(nums[0]));
+	}
+
+	return (nums[0]);
 }
 
 bool ClosestToZero::myCompare(int i, int j)
--- Leetcode/algomap-io/arrays_and_strings/Main.cpp
+++ Leetcode/algomap-io/arrays_and_strings/Main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
 	ClosestToZero ctz;
-	vector<int> nums = {10, -4, -2, 18451, 4, 8};
+	vector<int> nums = {10, -4, -2, 18451, 2, -2, 4, 8};
 
 	cout << ctz.findClosestNumber(nums);
 
