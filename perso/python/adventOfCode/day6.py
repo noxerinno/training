4333caae2e792876dbf600eaa76882e213b3f6da
Noxerinno <frantz.darbon@telecomnancy.net>
2024-12-11 20:51:51 +0100

    [Advent Of Code][WIP] day 6

--- perso/python/adventOfCode/day6.py
+++ perso/python/adventOfCode/day6.py
@@ -5,11 +5,12 @@ mazeWidth = 0
 currentX = 0
 currentY = 0
 direction = "UP"
+isNextStepAChangeOfDirection = False
 count = 0
 result = 0
 
 # Reading maze
-with open("rsc/day6.txt", 'r') as inputFile:
+with open("rsc/day6_test.txt", 'r') as inputFile:
     for line in inputFile.read().split('\n'):
         if len(line) > 0:
             if '^' in line:
@@ -38,23 +39,44 @@ def isNextStepOutOfMaze(x, y):
 # Checking if next step is obstructed, if so, changing direction
 def isNextStepObstructed(x, y):
     global direction
+    global isNextStepAChangeOfDirection
 
-    if direction == "UP" and maze[x-1][y] == '#':
-        direction = "RIGHT"
-        return True
+    if direction == "UP":
+        if x-2 >= 0 and maze[x-2][y] == '#':
+            isNextStepAChangeOfDirection = True
+            return
 
-    if direction == "RIGHT" and maze[x][y+1] == '#':
-        direction = "DOWN"
-        return True
+        if maze[x-1][y] == '#':
+            direction = "RIGHT"
+            return
+
+    if direction == "RIGHT":
+        if y+2 < mazeWidth and maze[x][y+2] == '#':
+            isNextStepAChangeOfDirection = True
+            return
+
+        if maze[x][y+1] == '#':
+            direction = "DOWN"
+            return
     
-    if direction == "DOWN" and maze[x+1][y] == '#':
-        direction = "LEFT"
-        return True
+    if direction == "DOWN":
+        if x+2 < mazeHeight and maze[x+2][y] == '#':
+            isNextStepAChangeOfDirection = True
+            return
 
-    if direction == "LEFT" and maze[x][y-1] == '#':
-        direction = "UP"
-        return True
+        if maze[x+1][y] == '#':
+            direction = "LEFT"
+            return
 
+    if direction == "LEFT":
+        if y-2 >= 0 and maze[x][y-2] == '#':
+            isNextStepAChangeOfDirection = True
+            return
+        
+        if maze[x][y-1] == '#':
+            direction = "UP"
+            return
+    
     return False
 
 # Take a new step
@@ -64,16 +86,48 @@ def takeStep():
 
     if direction == "UP":
         currentX -= 1
+        return
 
     if direction == "RIGHT":
         currentY += 1
+        return
 
     if direction == "DOWN":
         currentX += 1
+        return
 
     if direction == "LEFT":
         currentY -= 1
+        return
 
+
+## Problem 2
+loopCounter = 0
+
+def displayPath(x, y):
+    global isNextStepAChangeOfDirection
+    global loopCounter
+
+    if maze[x][y] == '^':
+        return
+
+    if isNextStepAChangeOfDirection or maze[x][y] == '|' or maze[x][y] == '-':
+        maze[x] = maze[x][:y] + '+' + maze[x][y+1:]
+        isNextStepAChangeOfDirection = False
+        return
+
+    if maze[x][y] == '|' or maze[x][y] == '-':
+        maze[x] = maze[x][:y] + '+' + maze[x][y+1:]
+        loopCounter += 1
+        return
+
+    if direction == "UP" or direction == "DOWN":
+        maze[x] = maze[x][:y] + '|' + maze[x][y+1:]
+        return
+
+    if direction == "LEFT" or direction == "RIGHT":
+        maze[x] = maze[x][:y] + '-' + maze[x][y+1:]
+        return
 
 # Setting up maze dimensions
 mazeHeight = len(maze)
@@ -81,8 +135,9 @@ mazeWidth = len(maze[0])
 count = 0
 
 while(True):
-    maze[currentX] = maze[currentX][:currentY] + 'X' + maze[currentX][currentY+1:]
     count += 1
+
+    displayPath(currentX, currentY)
      
     # While True loop base case
     if isNextStepOutOfMaze(currentX, currentY) or count > 100000:
@@ -96,6 +151,6 @@ while(True):
 
 for line in maze:
     print(line)
-    result += line.count('X')
+    # result += line.count('X')
 
-print(result)
+print(loopCounter)
