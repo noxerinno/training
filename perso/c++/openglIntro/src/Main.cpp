610d698dc25242b1e3a3f6fe0e476f1c400bd4da
Noxerinno <frantz.darbon@telecomnancy.net>
2025-01-28 17:27:13 +0100

    [OpenGL Intro] Shader creation and linking support

--- perso/c++/openglIntro/src/Main.cpp
+++ perso/c++/openglIntro/src/Main.cpp
@@ -4,10 +4,52 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-#include <ostream>
+#include <string>
 
-int main(void)
-{
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    /* Compilation error handeling */
+    int compilStatus;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &compilStatus);                                // Retreiving compilation status
+
+    if (compilStatus == GL_FALSE) {                                                     // If compilation failed
+        int errMessageLength;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &errMessageLength);                       // We retreive the error message length,
+        char errMessage[errMessageLength];                                              // create a var for the error message
+        glGetShaderInfoLog(id, errMessageLength, &errMessageLength, errMessage);        // and retreive the error message itself
+        
+        std::cout << "Failed to compile" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader !" << std::endl;
+        std::cout << errMessage << std::endl;
+
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);    
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    
+    return program;
+}
+
+int main(void) {
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -16,8 +58,7 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!window) {
         glfwTerminate();
         return -1;
     }
@@ -36,23 +77,25 @@ int main(void)
     };
 
     unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glGenBuffers(1, &buffer);                                                       // Create buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);                                          // "Select" my buffer as the current buffer
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);    // Add my vertices' data to the buffer
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);           // Describe my vertices' attribute
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        // glBegin(GL_TRIANGLES);
+        // glVertex2f(-0.5f, -0.5f);
+        // glVertex2f(0.0f, 0.5f);
+        // glVertex2f(0.5f, -0.5f);
+        // glEnd();
 
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
