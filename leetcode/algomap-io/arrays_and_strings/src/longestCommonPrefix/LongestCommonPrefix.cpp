ad13ddb1e485efc2b469742181ff8952f58aef89
Noxerinno <frantz.darbon@telecomnancy.net>
2025-07-07 15:12:17 +0200

    [LeetCode] Longest common prefix

---
+++ leetcode/algomap-io/arrays_and_strings/src/longestCommonPrefix/LongestCommonPrefix.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+
+#include "LongestCommonPrefix.h"
+
+using namespace std;
+
+LongestCommonPrefix::LongestCommonPrefix() {}
+
+string LongestCommonPrefix::longestCommonPrefix(vector<string>& strs) {
+    // Remove duplicates 
+    strs.erase(unique(strs.begin(), strs.end()), strs.end());
+
+    string longestPrefix = *strs.begin();
+    
+    for (string str : strs) {
+        if (str.empty()) {return str;}
+        
+        string prefix = "";
+        
+        for (size_t i = 0; i < str.length(); i++) {
+            if (str[i] != longestPrefix[i]) {break;}
+            
+            prefix += str[i];
+        }
+        
+        if (prefix.length() < longestPrefix.length()) {longestPrefix = prefix;}
+        
+        if (longestPrefix.empty()) {break;}
+    }
+
+    return longestPrefix;
+}
---
+++ leetcode/algomap-io/arrays_and_strings/src/longestCommonPrefix/LongestCommonPrefix.h
@@ -0,0 +1,15 @@
+#ifndef LONGESTCOMMONPREFIX_H
+#define LONGESTCOMMONPREFIX_H
+
+#include <string>
+#include <vector>
+
+class LongestCommonPrefix
+{
+public:
+    LongestCommonPrefix();
+    std::string longestCommonPrefix(std::vector<std::string>& strs);
+};
+
+
+#endif  // LONGESTCOMMONPREFIX_H
