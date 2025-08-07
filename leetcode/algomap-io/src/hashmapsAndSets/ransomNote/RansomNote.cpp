f9333ccbea16062c5d73f5aa0a8392ba95c12ee0
Noxerinno <frantz.darbon@telecomnancy.net>
2025-08-07 19:08:47 +0200

    [LeetCode] Ransom note Two solutions : maps and arrays

--- leetcode/algomap-io/src/hashmapsAndSets/ransomNote/RansomNote.cpp
+++ leetcode/algomap-io/src/hashmapsAndSets/ransomNote/RansomNote.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <vector>
 
 #include "RansomNote.hpp"
 
@@ -7,25 +8,57 @@ using namespace std;
 
 RansomNote::RansomNote() {}
 
+// bool RansomNote::canConstruct(string ransomNote, string magazine) {
+//     if (ransomNote.empty() || magazine.empty()) {
+//         return false;
+//     }
+
+//     // Mapping the ransom notes
+//     // auto ransomNoteMap = map<char, int>{};
+//     // for (char note : ransomNote) {
+//     //     ransomNoteMap[note]++;
+//     // }
+
+//     // Mapping the magazine
+//     auto magazineMap = map<char, int>{};
+//     for (char letter : magazine) {
+//         magazineMap[letter]++;
+//     }
+
+//     // for (map<char,int>::iterator it = ransomNoteMap.begin(); it != ransomNoteMap.end(); ++it) {
+//     //     if (magazineMap.find(it->first) == magazineMap.end()|| magazineMap[it->first] < ransomNoteMap[it->first]) {
+//     //         return false;
+//     //     }
+//     // }
+
+//     for (char note : ransomNote) {
+//         if (magazineMap[note] > 0) {
+//             magazineMap[note]--;
+//         } else {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
 bool RansomNote::canConstruct(string ransomNote, string magazine) {
     if (ransomNote.empty() || magazine.empty()) {
         return false;
     }
 
-    // Mapping the ransom notes
-    auto ransomNoteMap = map<char, int>{};
-    for (char note : ransomNote) {
-        ransomNoteMap[note]++;
+    vector<int> frequency(26);
+
+    for (const char& ch : ransomNote) {
+        frequency[ch - 'a']++;
     }
 
-    // Mapping the magazine
-    auto magazineMap = map<char, int>{};
-    for (char letter : magazine) {
-        magazineMap[letter]++;
+    for (const char& ch : magazine) {
+        frequency[ch - 'a']--;
     }
 
-    for (map<char,int>::iterator it = ransomNoteMap.begin(); it != ransomNoteMap.end(); ++it) {
-        if (magazineMap.find(it->first) == magazineMap.end()|| magazineMap[it->first] < ransomNoteMap[it->first]) {
+    for (const int& value : frequency) {
+        if (value > 0) {
             return false;
         }
     }
