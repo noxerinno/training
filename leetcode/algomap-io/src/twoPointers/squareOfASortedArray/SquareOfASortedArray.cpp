1d519282e9c185e206d08ad99f191322218653f2
Noxerinno <frantz.darbon@telecomnancy.net>
2025-08-15 16:57:53 +0200

    [LeetCode][WIP] Square of a sorted array

---
+++ leetcode/algomap-io/src/twoPointers/squareOfASortedArray/SquareOfASortedArray.cpp
@@ -0,0 +1,7 @@
+#include "SquareOfASortedArray.hpp"
+
+using namespace std;
+
+vector<int> SquareOfASortedArray::sortedSquares(vector<int>& nums) {
+    
+}
---
+++ leetcode/algomap-io/src/twoPointers/squareOfASortedArray/SquareOfASortedArray.hpp
@@ -0,0 +1,14 @@
+#ifndef SQUAREOFASORTEDARRAY_HPP
+#define SQUAREOFASORTEDARRAY_HPP
+
+#include <vector>
+
+class SquareOfASortedArray {
+public:
+    SquareOfASortedArray();
+    std::vector<int> sortedSquares(std::vector<int>& nums);
+};
+
+SquareOfASortedArray::SquareOfASortedArray() {}
+
+#endif  // SQUAREOFASORTEDARRAY_HPP
